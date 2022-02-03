777c4f89f748bff5424602f332c9652bde40a03e
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-02-04 00:06:05 +0100

    first plot with R works

--- example/01_gam/gen01.lisp
+++ example/01_gam/gen01.lisp
@@ -12,14 +12,33 @@
   (defparameter *repo-dir-on-host* "/home/martin/stage/cl-r-generator")
   (defparameter *repo-dir-on-github* "https://github.com/plops/cl-r-generator/tree/master/")
   (defparameter *example-subdir* "example/01_gam")
-  (defparameter *path* (format nil "~a/~a" *repo-dir-on-host* *example-subdir*) )
+  (defparameter *path* (format nil "~a/~a" *repo-dir-on-host* *example-subdir*))
+  (let ((show-counter 1))
+    (defun show (name code)
+      (prog1
+       `(do0
+	 (png (string ,(format nil "~2,'0d_~a.png" show-counter name)))
+	 ,code
+	 (dev.off))
+	(incf show-counter))))
   (write-source (format nil "~a/source/run01" *path*)
 		`(do0
 		  (comments "Simon Wood Generalized Additive Models, Ch7 GAMs in Practice, p. 325")
 		  (library mgcv)
+		  (library MASS)
 		  (setf sm (aref (smoothCon (s times :k 10)
 					    :data mcycle :knots NULL)
-				 (list 1))))))
+				 (list 1))
+			beta (coef (lm (~ mcycle$accel (- sm$X 1)))))
+		  
+		  ,(show "cycle"
+			 `(do0
+			   (with mcycle (plot times accel))
+			   (do0 (setf times (seq 0 60 :length 200)
+			     Xp (PredictMat sm (data.frame :times times)))
+		       (lines times (%*% Xp beta)))))
+		  
+		  )))
 
 
 
