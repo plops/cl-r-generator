9df7671a948c46cf8c1b53dae94d95188ab28679
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-02-05 11:28:54 +0100

    shape looks the same

--- example/01_gam/gen02.lisp
+++ example/01_gam/gen02.lisp
@@ -54,10 +54,14 @@
 				 :length 200)
 			  Xp (tf.X s sj)
 			  )
-		    (plot size wear)
-		    (lines s (%*% Xp (coef b))))
+		    (do0
+		     (comments "plot data and approximation using tent functions"
+			       "smoothness is uncontrolled")
+		     (plot size wear)
+		     (lines s (%*% Xp (coef b)))))
 		   (do0
 		    (defun prs.fit (y x xj sp)
+		      (comments "penalized fit with smoothing parameter sp")
 		      (setf X (tf.X x xj)
 			    D (diff (diag (length xj))
 				    :differences 2)
@@ -65,19 +69,61 @@
 					  D))
 			    y (c y (rep 0 (nrow D))))
 		      (lm (~ y (- X 1))))
-		    (do0 
+		    (do0
 		     (setf sj (seq (min size)
 				   (max size)
-				   :length 20)
+				   :length 80)
+			   s (seq (min size)
+				  (max size)
+				  :length 200)
 			   sp 2
 			   b (prs.fit wear
 				      size sj sp)
 			   )
-		     (plot size wear)
-		     (setf Xp (tf.X s sj))
-		     (lines s (%*% Xp (coef b))))
+		     (do0
 
-		    )
+		      (plot size wear)
+		      (setf Xp (tf.X s sj))
+		      (lines s (%*% Xp (coef b)))))
+		    (do0
+		     (comments "search good smoothing parameter using generalized cross validation (GCV) score")
+		     (setf n_grid 90
+			   rho (seq -9 11 :length n_grid)
+			   n (length wear)
+			   V (rep NA n_grid))
+		     (for (i (slice 1 n_grid))
+
+			  (setf b (prs.fit wear size sj (exp (aref rho i)))
+				trF (sum ($ (influence b)
+					    (aref hat (slice 1 n))))
+				rss (sum (^ (- wear
+					       (aref (fitted b) (slice 1 n)))
+					    2))
+				(aref V i) (/ (* n rss)
+					      (^ (- n trF)
+						 2)))
+			  (comments "b    .. fit model"
+				    "trF  .. extracted EDF (effective degrees of freedom)"
+				    "rss  .. residual summed squares"
+				    "V    .. generalized cross validation score"
+				    "influence$hat .. vector containing diagonal of the hat matrix for regression (leave-one-out deletion) diagnostics"))
+		     (do0
+		      (plot rho V :type (string "l")
+			    :xlab (expression (log lambda))
+			    :main (string "generalized cross validation score"))
+		      (grid)
+		      (do0
+		       (comments "plot with optimal lambda")
+		       (setf sp (exp (aref rho (== V (min V))))
+			     b (prs.fit wear size sj sp)
+			     )
+		       ,(show "optimal_GCV"
+			      `(do0
+				(plot size wear :main (paste (string  "GCV optimal fit #knots=")
+							     (length sj)))
+				(lines s (%*% Xp (coef b)))
+				(grid))))
+		      )))
 		   ))))
 
 
